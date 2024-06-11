d176a8e86aa0de286a0a2a42ba0d5fa472aacb50
ID-JA <idaissa93@gmail.com>
2024-06-11 17:06:52 +0100

    add project info

--- xmltoys-desktop/src/App.tsx
+++ xmltoys-desktop/src/App.tsx
@@ -15,6 +15,7 @@ type ComponentType = {
   "xml-to-json": () => JSX.Element;
   "validate-xml": () => JSX.Element;
   "xml-diff-checker": () => JSX.Element;
+  default: () => JSX.Element;
 };
 
 const Components = {
@@ -24,16 +25,23 @@ const Components = {
   "xml-to-json": XmlToJson,
   "validate-xml": ValidationXML,
   "xml-diff-checker": XmlDiffChecker,
+  default: Main,
 };
 
 const activeClasses = "bg-red-500 hover:bg-red-500";
 
 function App() {
-  const [state, setState] = useState("");
+  const [state, setState] = useState("default");
   const Component = Components[state as keyof ComponentType];
   return (
     <div className="container h-screen">
       <div className="flex items-center justify-center gap-4 p-4">
+        <Button
+          className={clsx(state === "default" && activeClasses)}
+          onClick={() => setState("default")}
+        >
+          Main Page
+        </Button>
         <Button
           className={clsx(state === "generate-dtd" && activeClasses)}
           onClick={() => setState("generate-dtd")}
@@ -73,6 +81,30 @@ function App() {
       </div>
       <br />
       {Component ? <Component /> : null}
+    </div>
+  );
+}
+
+function Main() {
+  return (
+    <div className="h-[500px] flex items-center justify-center">
+      <div>
+        <h1 className="text-3xl font-bold text-center mb-1">
+          XML Mini Projet (XmlToys)
+        </h1>
+        <p className="text-center text-md mb-5 max-w-[598px] mx-auto">
+          Une boîte à outils <strong>XML</strong> regroupe tous les outils
+          essentiels pour travailler avec <strong>XML</strong> en un seul
+          endroit pratique.
+        </p>
+        <h1 className="text-4xl font-bold text-center mb-4">
+          développé par: Nazha Haida et Jamal Id Aissa
+        </h1>
+        <h1 className="text-3xl font-semibold text-center mb-2">
+          LP LDW - 2023/2024 <br /> ENS Tétouan Université Abdelmalek Essaâdi
+        </h1>
+        <img src="./logo.png" className="h-28 mx-auto mt-4" />
+      </div>
     </div>
   );
 }
